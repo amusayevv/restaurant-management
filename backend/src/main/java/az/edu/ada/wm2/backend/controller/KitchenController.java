2bf5b5bb57eee71b053fd7f575e43459b2fd5f49
amusayev <alisamusa.ms@gmail.com>
2025-05-20 14:23:39 +0400

    thymeleaf

--- backend/src/main/java/az/edu/ada/wm2/backend/controller/KitchenController.java
+++ backend/src/main/java/az/edu/ada/wm2/backend/controller/KitchenController.java
@@ -1,15 +1,26 @@
 package az.edu.ada.wm2.backend.controller;
 
+import az.edu.ada.wm2.backend.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/kitchen")
 public class KitchenController {
+    OrderService orderService;
+
+    @Autowired
+    public KitchenController(OrderService orderService) {
+        this.orderService = orderService;
+    }
 
     @GetMapping
-    public String showKitchen() {
-        return "kitchen.html";
+    public String showKitchen(Model model) {
+        model.addAttribute("orders", orderService.getOrders());
+        return "kitchen";
     }
+
 }
